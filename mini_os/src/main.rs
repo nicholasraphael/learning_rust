e058bb0c3d3007ed19e6679a74647abf378f1d9e
Nicholas Raphael <n_raphael@live.com>
2020-06-30 19:48:48 -0400

    project: writing an OS in Rust

--- mini_os/src/main.rs
+++ mini_os/src/main.rs
@@ -2,6 +2,8 @@
 #![no_main] // disable all Rust level entry points
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World";
 
 /// This function is called on panic
--- mini_os/src/vga_buffer.rs
+++ mini_os/src/vga_buffer.rs
@@ -20,3 +20,28 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGTH: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGTH]
+}
